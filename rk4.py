e38a8f91e5bcd5862bac775fbbe0916240d27523
p-netm <pmuriuki98@gmail.com>
2019-05-26 13:40:20 +0300

    Overflow error handling

--- rk4.py
+++ rk4.py
@@ -1,28 +1,29 @@
 """
 Project1: fourth order Runge Kutta solution for second order ode as 
     a system of first oder ode's 
-_x, _y have been used as the names variable for x, y
-    respectively as not to override the 
-    Symbol('x'), and Symbol('y') variable names which are 
-    defined in the global scope as x and y
 """
-from fractions import Fraction
+import decimal
+from decimal import Decimal as dm
 import matplotlib.pyplot as plt
 import numpy as np
 from random import randint
+
+decimal.getcontext().prec = 100
 
 def f(mu, x, y):
     """ 
     x' = µx - y + xy**2
     """
-    return mu * x - y + x * y ** 2
+    x, y = dm(x), dm(y)
+    return mu * x - y + x * y ** dm(2)
 
 
 def g(mu, x, y):
     """ 
     y' = x + µy + y**3
     """
-    return x + mu * y + y ** 3
+    x, y = dm(x), dm(y)
+    return x + mu * y + y ** dm(3)
 
 def F(mu, Y, f1, f2):
     x, y = Y
@@ -89,7 +90,7 @@ def grid_plot(mu):
 
     Q = plt.quiver(X, Y, u, v, color='r')
 
-    plt.title("mu: {}".format(mu))
+    plt.title("mu: {}".format(float(mu)))
     plt.xlabel('$x$')
     plt.ylabel('$y$')
     plt.xlim([-2, 2])
@@ -111,7 +112,7 @@ def _plot(plot, initial_conditions, mu):
         plot.plot(x_points, y_points, 'b-') # path
     return plot
 
-def generate_init_conditions(steps=10, step_size=0.1):
+def generate_init_conditions(steps=100, step_size=0.1):
     """Generates one instance of the initial conditions 
     
     returns x(t) = x0, y(t) = y0, h(step_size) in range [0, 1]
@@ -120,7 +121,7 @@ def generate_init_conditions(steps=10, step_size=0.1):
     K, L = randint(0, 20), randint(0, 20)
     _x = -1 + 0.1 * K
     _y = -1 + 0.1 * L
-    return list(map(Fraction, [0, _x, _y, step_size, steps]))
+    return list(map(dm, [0, _x, _y, step_size, steps]))
 
 def aggregator(t, _x, _y, h, mu, steps):
     """
@@ -135,8 +136,11 @@ def aggregator(t, _x, _y, h, mu, steps):
         'y': [_y],
         'mu': mu
     }
-    for i in range(2):
-        response = rk4(mini_dict['t'][i], mini_dict['x'][i], mini_dict['y'][i], h, mu, f, g)
+    for i in range(int(steps)):
+        try:
+            response = rk4(mini_dict['t'][i], mini_dict['x'][i], mini_dict['y'][i], h, mu, f, g)
+        except decimal.Overflow:
+            break
         mini_dict['t'].append(response[0])
         mini_dict['x'].append(response[1])
         mini_dict['y'].append(response[2])
@@ -151,10 +155,9 @@ def main(n=10):
     """
     # a list of initial conditions 
     inits = [generate_init_conditions() for _ in range(n)]
-    
-    # i suggest we create a function that is given the plot object
-    # and the plot data and returns a plt object with the data embedded
-    for mu in [-1.0, -0.5, -0.2, 0.0, 0.1, 0.5, 1.0]:
+    mus = list(map(dm, [-1.0, -0.5, -0.2, 0.0, 0.1, 0.5, 1.0]))
+    #plotting
+    for mu in mus:
         # quiver plot
         plt = grid_plot(mu)
         # Embed plot points data
